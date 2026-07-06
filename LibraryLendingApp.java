a283ab73f20728e60bdc6b517c2c8d4f77284639
Atsuki Hasegawa <1610685046@campus.ouj.ac.jp>
2026-07-06 17:03:39 +0900

    Fix formatting issue in LibraryLendingApp class

--- LibraryLendingApp.java
+++ LibraryLendingApp.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class　LibraryLendingApp  {
+public class LibraryLendingApp  {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
